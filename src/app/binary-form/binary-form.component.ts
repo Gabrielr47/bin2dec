cda63a80854ccf251b91a1e76e59e98c87d97d0b
Gabriel Rodrigues <gabrielr47@gmail.com>
2020-02-17 22:43:05 +0100

    add form validation

--- src/app/binary-form/binary-form.component.ts
+++ src/app/binary-form/binary-form.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-binary-form',
   templateUrl: './binary-form.component.html',
   styleUrls: ['./binary-form.component.scss']
 })
-export class BinaryFormComponent implements OnInit {
+export class BinaryFormComponent {
+
+  binaryForm = new FormGroup({
+    binary: new FormControl('',
+      [
+        Validators.required,
+        Validators.pattern('^[0-1]{1,}$'),
+      ]
+    ),
+    decimal: new FormControl(''),
+  });
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit() {
+    this.binaryForm.get('decimal').setValue(parseInt(this.binaryForm.get('binary').value, 2));
   }
 
 }
